ccaccb38a9ef91d3b0b8e5e0d8399b529cb9d80e
Amit Suthar <amitsuthar4887@gmail.com>
2025-10-10 16:34:25 +0530

    Btn04 and Btn05

---
+++ 1-html-css/frontend-design-practice/01-components/01-buttons/components/Btn04.jsx
@@ -0,0 +1,10 @@
+const Btn04 = () => {
+  return (
+    <div className="bg-black text-white px-2 py-4">
+      <button className="block mx-auto bg-[#fff000] px-5 py-2.5 text-black font-serif font-semibold cursor-pointer rounded-xl hover:shadow-[0_0_5px_#fef08a] hover:scale-105 transition-all duration-200 hover:shadow-amber-50">
+        Hello !
+      </button>
+    </div>
+  );
+};
+export default Btn04;
--- 1-html-css/frontend-design-practice/01-components/01-buttons/src/App.jsx
+++ 1-html-css/frontend-design-practice/01-components/01-buttons/src/App.jsx
@@ -4,6 +4,8 @@ import "./index.css";
 import Btn01 from "../components/btn01";
 import Btn02 from "../components/btn02";
 import Btn03 from "../components/Btn03";
+import Btn04 from "../components/Btn04";
+import Btn05 from "../components/Btn05";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Btn01 />
         <Btn02 />
         <Btn03 />
+        <Btn04 />
+        <Btn05 />
       </div>
     </>
   );
