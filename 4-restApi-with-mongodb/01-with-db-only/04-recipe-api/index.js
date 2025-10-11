1b6de55cb3faf6416e9160e8fb09b25f1293105f
Amit Suthar <amitsuthar4887@gmail.com>
2025-10-11 09:59:09 +0530

    users routes

--- 4-restApi-with-mongodb/01-with-db-only/04-recipe-api/index.js
+++ 4-restApi-with-mongodb/01-with-db-only/04-recipe-api/index.js
@@ -36,6 +36,54 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+// GET /api/users/me` → Get user info
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+
+    res.status(200).json({
+      message: "Here's the list of all users: ",
+      users: users,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      message: "Something went wrong !",
+      error: err,
+    });
+  }
+});
+
+// * `PATCH /api/users/:id` → Update profile
+app.patch("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidator: true,
+    });
+
+    if (!user) {
+      return res.status(400).json({
+        message: `No user with id:${id} exists !`,
+      });
+    }
+
+    const updatedUser = await user.save();
+
+    res.status(200).json({
+      message: `Here is the updated version of user with id: ${id}: `,
+      user: updatedUser,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      message: "Something went wrong !",
+      error: err,
+    });
+  }
+});
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
